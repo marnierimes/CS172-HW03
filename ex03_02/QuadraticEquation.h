736c50dfa36aea971c182ffe473f6c7d865cddf9
mrimes19 <mrimes19@LAB-SCI-304-16>
2016-09-30 19:23:01 -0700

    Add project files.

---
+++ ex03_02/QuadraticEquation.h
@@ -0,0 +1,14 @@
+#pragma once
+//
+//
+
+class QuadraticEquation
+{
+public:
+	double a, b, c;
+
+	double getDiscriminant(int a, int b, int c);
+	double getRoot1(int a, int b, int c);
+	double getRoot2(int a, int b, int c);
+
+};
---
+++ ex03_02/Source.cpp
@@ -0,0 +1,17 @@
+//
+//Marnie Rimes
+//09-29-16
+// HW03 ex03_02
+// Liang exercise 9.6:Algeba: Quadratic equations
+//
+
+#include<iostream>
+#include"QuadraticEquation.h"
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "Enter in the values for a, b, and c ";
+	cin >> a >> b >> c;
+}
